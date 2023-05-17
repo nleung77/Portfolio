f5d219948f1dfebdd9b55a5c33c8cbb6eb1e69ca
nleung77 <nleung77@gmail.com>
2023-05-17 00:03:32 -0400

    cleaned file structure

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -43,13 +43,13 @@ const Skills = () => {
         </motion.div>
 
         <Skill name="HTML" x="-20vw" y="2vw" />
-        <Skill name="CSS" x="-5vw" y="-10vw" />
+        <Skill name="Posgresql" x="-5vw" y="-10vw" />
         <Skill name="Javascript" x="20vw" y="6vw" />
         <Skill name="ReactJS" x="0vw" y="12vw" />
         <Skill name="NextJS" x="-20vw" y="-15vw" />
         <Skill name="Quality Assurance" x="15vw" y="-12vw" />
         <Skill name="Github" x="-35vw" y="-5vw" />
-        <Skill name="Web Design" x="32vw" y="-5vw" />
+        <Skill name="Python" x="32vw" y="-5vw" />
         <Skill name="MongoDB" x="0vw" y="-20vw" />
         <Skill name="TypeScript" x="-25vw" y="18vw" />
         <Skill name="Tailwind CSS" x="28vw" y="18vw" />
